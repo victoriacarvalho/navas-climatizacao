cd58b6c3eb2841de98feb01ecbf41c2f88ff91e4
victoriacarvalho <victoriacostasilvac@gmail.com>
2025-07-08 07:00:34 -0300

    feat: add booking on transaction table

--- app/_actions/create-bookings.ts
+++ app/_actions/create-bookings.ts
@@ -18,14 +18,42 @@ export const createBooking = async (params: CreateBookingParams) => {
     throw new Error("Usuário não autenticado.")
   }
 
+  const service = await db.navasService.findUnique({
+    where: {
+      id: params.serviceId,
+    },
+    include: {
+      Navas: true,
+    },
+  })
+
+  if (!service) {
+    throw new Error("Serviço não encontrado.")
+  }
+
+  const transaction = await db.transaction.create({
+    data: {
+      name: service.name,
+      type: "DEPOSIT",
+      amount: service.price,
+      category: "SERVICE_PAYMENT",
+      // TODO: Obter o método de pagamento do usuário
+      paymentMethod: "OTHER",
+      date: params.date,
+      navasId: service.NavasId,
+    },
+  })
+
   await db.booking.create({
     data: {
       serviceId: params.serviceId,
       date: params.date,
       description: params.observacoes,
       userId: (user.user as any).id,
+      transactionId: transaction.id,
     },
   })
+
   revalidatePath("/navas[id]")
   revalidatePath("/bookings")
 }
